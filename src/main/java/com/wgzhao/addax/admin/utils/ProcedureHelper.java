c1acbb126c8d2a68674608d5ea443940493c38d6
wgzhao <wgzhao@gmail.com>
2025-04-11 10:22:13 +0800

    Remove unused import of FuncHelper in ProcedureHelper

--- src/main/java/com/wgzhao/addax/admin/utils/ProcedureHelper.java
+++ src/main/java/com/wgzhao/addax/admin/utils/ProcedureHelper.java
@@ -1,6 +1,5 @@
 package com.wgzhao.addax.admin.utils;
 
-import com.wgzhao.addax.admin.utils.FuncHelper;
 import jakarta.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
