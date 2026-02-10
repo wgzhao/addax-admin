20a1e1e8c48d5ffa3b241de562a387188a9feb90
wgzhao <wgzhao@gmail.com>
2026-02-10 15:17:08 +0800

    [feature] Update RiskLogRepository and RiskLogService to filter recent risks by date

--- backend/src/main/java/com/wgzhao/addax/admin/repository/RiskLogRepository.java
+++ backend/src/main/java/com/wgzhao/addax/admin/repository/RiskLogRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface RiskLogRepository
     extends JpaRepository<RiskLog, Long>
 {
-    List<RiskLog> findAllByOrderByCreatedAtDesc(Pageable pageable);
+    List<RiskLog> findAllByCreatedAtAfterOrderByCreatedAtDesc(LocalDateTime localDateTime, Pageable pageable);
 }
