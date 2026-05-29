2b5412903a3c9e69010130ee9ba904a779b62966
wgzhao <wgzhao@gmail.com>
2026-05-29 09:16:21 +0800

    chore(repo): relax subject-case to warning level

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
       ['fe', 'be', 'api', 'db', 'infra', 'repo', 'deps'],
     ],
     'scope-empty': [2, 'never'],
-    'subject-case': [2, 'always', ['lower-case']],
+    'subject-case': [1, 'always', ['lower-case']],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
   },
