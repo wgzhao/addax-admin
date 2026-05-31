90f37dcdcc676b0bcfde99b006f7efb675c1e7e8
wgzhao <wgzhao@gmail.com>
2026-05-31 09:24:00 +0800

    chore(frontend): add Prettier + lint-staged config and enforce formatting on pre-commit
    Why: avoid different editor formatters causing noisy commits and enforce consistent frontend formatting.
    
    What:
    - Add .prettierrc.cjs and .prettierignore at repo root
    - Add prettier and lint-staged to frontend devDependencies and configure lint-staged
    - Update root package.json lint-staged config
    - Update husky pre-commit hook to run lint-staged for frontend
    - Add formatting scripts to frontend package.json
    
    Note: dependencies need to be installed (yarn --cwd frontend install).

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'lf'
+};
